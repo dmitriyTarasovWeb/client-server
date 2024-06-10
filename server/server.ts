843813fe476c09b87fd821ee762e782233a7621b
dmitriytarasovweb <tarasov14.08.2002@gmail.com>
2024-06-11 00:24:50 +0300

    change default song

--- server/server.ts
+++ server/server.ts
@@ -17,10 +17,10 @@ connectMongoDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
-let allowedOrigin = "http://localhost:5000";
+let allowedOrigin = "http://localhost:3000";
 
 if (process.env.NODE_ENV === "production") {
-  allowedOrigin = "http://localhost:5000";
+  allowedOrigin = "http://localhost:3000";
 }
 
 const corsOptions = {
